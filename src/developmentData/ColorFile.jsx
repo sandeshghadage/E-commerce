ae12371e8ee4f6498ebb8942184f182d0a09dfc2
sandeshghadage <sandeshghadage3@gmail.com>
2023-05-19 22:16:23 +0530

    style theme created

---
+++ src/developmentData/ColorFile.jsx
@@ -0,0 +1,41 @@
+// import React from "react";
+
+// export default function ColorFile() {
+//   return (
+//     <div style={{ display: "flex" }}>
+//       <div style={{ display: "flex", flexDirection: "column", gap: "10px" }}>
+//         <Box color={"#073642"} name="primary: #073642" />
+//         <Box color={"#094959"} name="primaryHover: #094959" />
+//         <Box color={"#7D6005"} name="secondary: #7D6005" />
+//         <Box color={"#55AE51"} name="menuBarColor: #55AE51" />
+//         <Box color={"#1a181e"} name="black12: #1a181e" />
+//         <Box color={"#353535"} name="black25: #353535" />
+//         <Box color={"#4d4d4d"} name="black30: #4d4d4d" />
+//         <Box color={"grey"} name="black50: grey" />
+//         <Box color={"#989898"} name="black55: #989898" />
+//         <Box color={"#999"} name="black60: #999" />
+//         <Box color={"#b2b2b2"} name="black70: #b2b2b2" />
+//       </div>
+//       <div style={{ display: "flex", flexDirection: "column", gap: "10px" }}>
+//         <Box color={"#ccc"} name="black80: #ccc" />
+//         <Box color={"#d9d9d9"} name="black85: #d9d9d9" />
+//         <Box color={"#e6e6e6"} name="black90: #e6e6e6" />
+//         <Box color={"#f2f2f2"} name="black95: #f2f2f2" />
+//         <Box color={"#f5f5f5"} name="black96: #f5f5f5" />
+//         <Box color={"#fafafa"} name="black98: #fafafa" />
+//         <Box color={"#17b31b"} name="secondaryGreen: #17b31b" />
+//         <Box color={"#149917"} name="greenHover: #149917" />
+//         <Box color={"#cc0a1d"} name="redHover: #cc0a1d" />
+//       </div>
+//     </div>
+//   );
+// }
+
+// function Box({ color, name }) {
+//     return (
+//       <div style={{ display: "flex", gap: "10px", alignItems: "center" }}>
+//         <div className="box" style={{ backgroundColor: color }}></div>
+//         <div>{name}</div>
+//       </div>
+//     );
+//   }
